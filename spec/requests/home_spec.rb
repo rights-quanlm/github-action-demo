5648bb3d931ddeafec0397410c2c894be04eafe5
Le Minh Quan <leminh.quan@rights-s.jp>
2022-05-24 03:56:51 +0900

    [fix] tests

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "HomeController", type: :request do
   describe '"version" method' do
-    expected = '1.0.2-beta'
+    expected = '1.0.20-beta (2f6d887c)'
 
     it "should return #{expected}" do
       expect(HomeController.new.version).to eq(expected)
